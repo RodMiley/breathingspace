04ae03b949ccfbe7fffc106f442c6c944891131d
Rod Miley <rodmiley5@hotmail.com>
2020-09-25 13:06:03 +0100

    creditor - alternative account page

--- app/views/advisor-latest/_routes.js
+++ app/views/advisor-latest/_routes.js
@@ -2,12 +2,22 @@ const express = require('express');
 const router = express.Router();
 
 var searchClient = require('./search.json')
+var maClient = require('./data/maClients.js')
+
 
      router.get('/search-results-page', function (req, res, next) {
       res.locals.clients = searchClient
       next()
        })
-  
+
+       router.get('/account-page-active', function (req, res, next) {
+          res.locals.customers = maClient
+          next()
+           })
+         
+   
+
+
 
 
 module.exports = router
