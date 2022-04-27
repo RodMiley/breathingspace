e67aaece29c3584d3bd5fced6fc10fd0d26e82fc
RodMiley <rodmiley5@hotmail.com>
2022-04-27 11:53:38 +0100

    added new money advisor pt

--- app/views/sdrp/current/data/dapcases.js
+++ app/views/sdrp/current/data/dapcases.js
@@ -2,7 +2,7 @@ module.exports =[{"name":"Ginnie Atrill","reference":"SDRP619910","task":"Review
 {"name":"Armstrong Hadenton","reference":"SDRP904312","task":"Confirm sold debt","status":"Cnfirm debts"},
 {"name":"Kerwin Bramley","reference":"SDRP823471","task":"Review updated debt details","status":"Confirm debts"},
 {"name":"Nicolina Musterd","reference":"SDRP250150","task":"Draft plans nearing deadline","status":"Plan due"},
-{"name":"Sonnie Hamlington","reference":"SDRP058835","task":"Review updated debt details","status":"Plan due"},
+{"name":"Sonnie Hamlington","reference":"SDRP058835","task":"Review confirmed plan","status":"Confirmed plan"},
 {"name":"Bette Rowbrey","reference":"SDRP023446","task":"Draft plans nearing deadline","status":"Plan due"},
 {"name":"Pacorro Turpey","reference":"SDRP332966","task":"Review updated debt details","status":"Agreed plan"},
 {"name":"Dorette Tassel","reference":"SDRP726245","task":"Draft plans nearing deadline","status":"Plan due"},
