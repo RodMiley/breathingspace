1acd197980a6edf9b4bf57b6f4a086239c34f88e
RodMiley <rodmiley5@hotmail.com>
2022-04-29 13:11:13 +0100

    added creditor objection scenario

--- app/views/sdrp/current/data/dapcases.js
+++ app/views/sdrp/current/data/dapcases.js
@@ -3,7 +3,7 @@ module.exports =[{"name":"Ginnie Atrill","reference":"SDRP619910","task":"Review
 {"name":"Kerwin Bramley","reference":"SDRP823471","task":"Review updated debt details","status":"Confirm debts"},
 {"name":"Nicolina Musterd","reference":"SDRP250150","task":"Draft plans nearing deadline","status":"Plan due"},
 {"name":"Sonnie Hamlington","reference":"SDRP058835","task":"Review confirmed plan","status":"Confirmed plan"},
-{"name":"Bette Rowbrey","reference":"SDRP023446","task":"Draft plans nearing deadline","status":"Plan due"},
+{"name":"Bette Rowbrey","reference":"SDRP023446","task":"Creditor objects to plan","status":"Creditor objection"},
 {"name":"Pacorro Turpey","reference":"SDRP332966","task":"Review updated debt details","status":"Agreed plan"},
 {"name":"Dorette Tassel","reference":"SDRP726245","task":"Draft plans nearing deadline","status":"Plan due"},
 {"name":"Lara Hiddersley","reference":"SDRP414771", "task":"New message","status":"Confirm debts"},
