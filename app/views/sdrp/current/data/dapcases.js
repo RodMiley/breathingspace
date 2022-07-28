0492dbf05a56418e6ad007d1ca5263a2dc474cd4
RodMiley <rodmiley5@hotmail.com>
2022-07-28 10:23:04 +0100

    added incriment 1 static screens

--- app/views/sdrp/current/data/dapcases.js
+++ app/views/sdrp/current/data/dapcases.js
@@ -1,10 +1,10 @@
-module.exports =[{"name":"Ginnie Atrill","reference":"SDRP619910","task":"Review new debts added by creditor","status":"Confirm debts"},
-{"name":"Armstrong Hadenton","reference":"SDRP904312","task":"Confirm sold debt","status":"Cnfirm debts"},
-{"name":"Kerwin Bramley","reference":"SDRP823471","task":"Review updated debt details","status":"Confirm debts"},
-{"name":"Nicolina Musterd","reference":"SDRP250150","task":"Draft plans nearing deadline","status":"Plan due"},
+module.exports =[{"name":"Ginnie Atrill","reference":"SDRP619910","task":"Review new debts added by creditor","status":"intention"},
+{"name":"Armstrong Hadenton","reference":"SDRP904312","task":"Confirm sold debt","status":"intention"},
+{"name":"Kerwin Bramley","reference":"SDRP823471","task":"Review updated debt details","status":"intention"},
+{"name":"Nicolina Musterd","reference":"SDRP250150","task":"Draft plans nearing deadline","status":"provisional"},
 {"name":"Sonnie Hamlington","reference":"SDRP058835","task":"Review confirmed plan","status":"Confirmed plan"},
 {"name":"Bette Rowbrey","reference":"SDRP023446","task":"Creditor objects to plan","status":"Creditor objection"},
-{"name":"Pacorro Turpey","reference":"SDRP332966","task":"Review updated debt details","status":"Agreed plan"},
-{"name":"Dorette Tassel","reference":"SDRP726245","task":"Draft plans nearing deadline","status":"Plan due"},
-{"name":"Lara Hiddersley","reference":"SDRP414771", "task":"New message","status":"Confirm debts"},
-{"name":"Darby Ruse","reference":"SDRP033227", "task":"New message","status":"Confirm debts",}]
+{"name":"Pacorro Turpey","reference":"SDRP332966","task":"Review updated debt details","status":"final"},
+{"name":"Dorette Tassel","reference":"SDRP726245","task":"Draft plans nearing deadline","status":"provisional"},
+{"name":"Lara Hiddersley","reference":"SDRP414771", "task":"New message","status":"intention"},
+{"name":"Darby Ruse","reference":"SDRP033227", "task":"New message","status":"intention",}]
